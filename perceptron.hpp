e7b5e8f99ab2f7563a18e62789127a2dfe4e6be6
happy-sloth <aloha.g133@gmail.com>
2025-04-27 13:03:31 +0300

    [*] Optimized CUDA code [+] Added exceptions instead of returning ret_code [+] Added math tests in first iteration in main

--- perceptron.hpp
+++ perceptron.hpp
@@ -14,12 +14,12 @@ private:
     unsigned hiddenNodes;
     unsigned outputNodes;
 
-    MatrixCUDA *wih = NULL, *who = NULL, *inputVector = NULL, *hiddenVector = NULL, *outputVector = NULL;
+    MatrixCUDA *wih = NULL, *who = NULL, *inputVector = NULL, *hiddenVector = NULL, *outputVector = NULL, *out1Vector = NULL, *hidden1Vector = NULL;
 
 public:
     explicit perceptron(unsigned inNodes_cnt, unsigned hidNodes_cnt, unsigned outNodes_cnt);
     ~perceptron();
 
-    int train(Matrix &input, Matrix &targetValue);
+    int train(const MatrixCUDA &input, const MatrixCUDA &targetValue);
     Matrix queue(Matrix &input);
 };
